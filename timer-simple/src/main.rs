5e0e6fbdb2057d720cb8155abca838f7dce85177
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2023-01-01 20:46:40 +0100

    Simple test with timer

---
+++ timer-simple/src/main.rs
@@ -0,0 +1,29 @@
+#![deny(unsafe_code)]
+#![no_main]
+#![no_std]
+
+use cortex_m::prelude::_embedded_hal_timer_CountDown;
+use cortex_m_rt::entry;
+use microbit::board::Board;
+use microbit::hal::timer::Timer;
+use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
+
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+
+    let board = Board::take().unwrap();
+    let _clocks = microbit::hal::clocks::Clocks::new(board.CLOCK).enable_ext_hfosc();
+
+    let mut timer = Timer::new(board.TIMER0);
+
+    timer.start(core::u32::MAX);
+    loop {
+        if timer.read() > 1_000_000 {
+            rprintln!("1 second");
+            timer.start(core::u32::MAX);
+        }
+    }
+}
